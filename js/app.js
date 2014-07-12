36664a60a652decd7e2fbda90581e8fc4973d96a
Mohammed Irshad Hasmat <irshad_no1@live.com>
2014-07-12 17:24:11 +0530

    Working on List

--- js/app.js
+++ js/app.js
@@ -86,6 +86,16 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
         }
       }
     })
+    
+    .state('tab.99namesList', {
+      url: '/99namesList',
+      views: {
+        'tab-dash': {
+          templateUrl: 'templates/tab-99names-List.html',
+          controller: '99NamesCtrl'
+        }
+      }
+    })
 
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/dash');
