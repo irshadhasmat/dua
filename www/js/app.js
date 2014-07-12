69054057a8c270473839ba660f5c545a19dd5c5f
Mohammed Irshad Hasmat <irshad_no1@live.com>
2014-07-12 12:31:58 +0530

    Trial

--- www/js/app.js
+++ www/js/app.js
@@ -3,21 +3,33 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('duaApp', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+        .run(function($ionicPlatform) {
+            $ionicPlatform.ready(function() {
+                // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+                // for form inputs)
+                if (window.cordova && window.cordova.plugins.Keyboard) {
+                    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                }
+                if (window.StatusBar) {
+                    StatusBar.styleDefault();
+                }
+            });
+        })
 
-.controller("99NamesCtrl",function($scope){
-	$scope.name = "Irshad";
-})
+        .config(function($stateProvider) {
+            $stateProvider
+                    .state('index', {
+                        url: '/',
+                        templateUrl: 'partial/99names.html'
+                    })
+                    .state('music', {
+                        url: '/music',
+                        templateUrl: 'music.html'
+                    });
+        })
+
+        .controller("99NamesCtrl", function($scope) {
+            $scope.name = "Irshad";
+        })
