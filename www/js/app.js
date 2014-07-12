706e19ee14fb1cae2573f3ce92c005086aa69729
Mohammed Irshad Hasmat <irshad_no1@live.com>
2014-07-12 12:44:10 +0530

    Something wrong in my system

--- www/js/app.js
+++ www/js/app.js
@@ -1,35 +1,68 @@
-// Ionic Starter App
+angular.module('ionicApp', ['ionic'])
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('duaApp', ['ionic'])
+.config(function($stateProvider, $urlRouterProvider) {
 
-        .run(function($ionicPlatform) {
-            $ionicPlatform.ready(function() {
-                // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-                // for form inputs)
-                if (window.cordova && window.cordova.plugins.Keyboard) {
-                    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-                }
-                if (window.StatusBar) {
-                    StatusBar.styleDefault();
-                }
-            });
-        })
+  $stateProvider
+    .state('tabs', {
+      url: "/tab",
+      abstract: true,
+      templateUrl: "tabs.html"
+    })
+    .state('tabs.home', {
+      url: "/home",
+      views: {
+        'home-tab': {
+          templateUrl: "home.html",
+          controller: 'HomeTabCtrl'
+        }
+      }
+    })
+    .state('tabs.facts', {
+      url: "/facts",
+      views: {
+        'home-tab': {
+          templateUrl: "facts.html"
+        }
+      }
+    })
+    .state('tabs.facts2', {
+      url: "/facts2",
+      views: {
+        'home-tab': {
+          templateUrl: "facts2.html"
+        }
+      }
+    })
+    .state('tabs.about', {
+      url: "/about",
+      views: {
+        'about-tab': {
+          templateUrl: "about.html"
+        }
+      }
+    })
+    .state('tabs.navstack', {
+      url: "/navstack",
+      views: {
+        'about-tab': {
+          templateUrl: "nav-stack.html"
+        }
+      }
+    })
+    .state('tabs.contact', {
+      url: "/contact",
+      views: {
+        'contact-tab': {
+          templateUrl: "contact.html"
+        }
+      }
+    });
 
-        .config(function($stateProvider) {
-            $stateProvider
-                    .state('index', {
-                        url: '/',
-                        templateUrl: 'partial/99names.html'
-                    })
-                    .state('music', {
-                        url: '/music',
-                        templateUrl: 'music.html'
-                    });
-        })
 
-        .controller("99NamesCtrl", function($scope) {
-            $scope.name = "Irshad";
-        })
+   $urlRouterProvider.otherwise("/tab/home");
+
+})
+
+.controller('HomeTabCtrl', function($scope) {
+  console.log('HomeTabCtrl');
+});
