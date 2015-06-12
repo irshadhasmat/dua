bcba7233321556b863faf7c689f87641491d5d73
Mohammed Irshad Hasmat <irshad_no1@live.com>
2015-06-12 23:54:15 +0530

    Restarted  with Dua

--- platforms/android/assets/www/js/controllers.js
+++ platforms/android/assets/www/js/controllers.js
@@ -1,7 +1,7 @@
 angular.module('starter.controllers', [])
 
         .controller('DashCtrl', function($scope) {
-            
+
         })
 
         .controller('FriendsCtrl', function($scope, Friends) {
@@ -16,39 +16,32 @@ angular.module('starter.controllers', [])
         })
 
         .controller('DuroodCtrl', function($scope) {
-//        $scope.durood =
-//                [{'id':'1', 'name':'Durood Ibrahim', 'ArabicText':'اللَّهُمَّ صَلِّ عَلَى مُحَمَّدٍ وَعَلَى آلِ مُحَمَّدٍ
-//                        كَمَا صَلَّيْتَ عَلَى إِبْرَاهِيمَ وَعَلَى آلِ إِبْرَاهِيمَ
-//                        .إِنَّكَ حَمِيدٌ مَجِيدٌ
-//                        اللَّهُمَّ بَارِكْ عَلَى مُحَمَّدٍ، وَعَلَى آلِ مُحَمَّدٍ
-//                        كَمَا بَارَكْتَ عَلَى إِبْرَاهِيمَ وَعَلَى آلِ إِبْرَاهِيمَ
-//                        .إِنَّكَ حَمِيدٌ مَجِيدٌ'}]
         })
 
         .controller('99NamesCtrl', function($scope, NamesOfAllah) {
             $scope.Names = NamesOfAllah.all();
         })
 
-        .controller('TasbeehCtrl', function($scope, $cordovaVibration,$cordovaDevice) {
+        .controller('TasbeehCtrl', function($scope, $cordovaVibration, $cordovaDevice) {
             $scope.count = 0;
             $scope.tasbeeh = "سُبْحَانَ اللّهِ";
             $scope.inEnglish = "Subhaanallah";
             $scope.onTap = function() {
                 $scope.count++;
 
-                if($scope.count == 33)
+                if ($scope.count == 33)
                 {
                     $scope.tasbeeh = "الْحَمْدُللّهِ";
                     $scope.inEnglish = "Alhamdulillah";
                     $cordovaVibration.vibrate(50);
                 }
-                else if($scope.count == 66)
+                else if ($scope.count == 66)
                 {
                     $scope.tasbeeh = "الله أكبر";
                     $scope.inEnglish = "Allahu Akbar";
                     $cordovaVibration.vibrate(50);
                 }
-                else if($scope.count == 100)
+                else if ($scope.count == 100)
                 {
                     $cordovaVibration.vibrate(1000);
                     $scope.count = 0;
